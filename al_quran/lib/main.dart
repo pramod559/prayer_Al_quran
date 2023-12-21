a3ac0aa346364554679e9e2450a5dfa1611a31cb
pramod <kumarpramod559@gmail.com>
2023-12-21 21:01:13 +0530

    test

--- al_quran/lib/main.dart
+++ al_quran/lib/main.dart
@@ -1,125 +1,113 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:provider/provider.dart';
+import 'package:url_strategy/url_strategy.dart';
 
-void main() {
+import 'package:al_quran/app_routes.dart';
+import 'package:al_quran/cubits/bookmarks/cubit.dart';
+import 'package:al_quran/cubits/chapter/cubit.dart';
+import 'package:al_quran/cubits/juz/cubit.dart';
+import 'package:al_quran/models/ayah/ayah.dart';
+import 'package:al_quran/models/chapter/chapter.dart';
+import 'package:al_quran/models/juz/juz.dart';
+import 'package:al_quran/providers/app_provider.dart';
+import 'package:al_quran/providers/onboarding_provider.dart';
+import 'package:al_quran/screens/bookmarks/bookmarks_screen.dart';
+import 'package:al_quran/screens/home/home_screen.dart';
+import 'package:al_quran/screens/juz/juz_index_screen.dart';
+import 'package:al_quran/screens/onboarding/onboarding.dart';
+import 'package:al_quran/screens/share_app/share_app.dart';
+import 'package:al_quran/screens/splash/splash.dart';
+import 'package:al_quran/screens/surah/surah_index_screen.dart';
+
+import 'configs/core_theme.dart' as theme;
+
+Future<void> main() async {
+  setPathUrlStrategy();
+  WidgetsFlutterBinding.ensureInitialized();
+
+// hive
+  await Hive.initFlutter();
+
+  Hive.registerAdapter<Juz>(JuzAdapter());
+  Hive.registerAdapter<Ayah>(AyahAdapter());
+  Hive.registerAdapter<Chapter>(ChapterAdapter());
+
+  await Hive.openBox('app');
+  await Hive.openBox('data');
+
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  MyAppState createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+
+    return MultiProvider(
+      providers: [
+        BlocProvider(create: (_) => JuzCubit()),
+        BlocProvider(create: (_) => ChapterCubit()),
+        BlocProvider(create: (_) => BookmarkCubit()),
+        ChangeNotifierProvider(create: (_) => AppProvider()),
+        ChangeNotifierProvider(create: (_) => OnBoardingProvider()),
+      ],
+      child: Consumer<AppProvider>(
+        builder: ((context, value, child) {
+          return MaterialChild(
+            value: value,
+          );
+        }),
+      ),
+    );
+  }
+}
+
+class MaterialChild extends StatelessWidget {
+  final AppProvider? value;
+  const MaterialChild({
+    Key? key,
+    this.value,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      title: 'They Holy Qur\'an',
+      debugShowCheckedModeBanner: false,
+      theme: theme.themeLight,
+      darkTheme: theme.themeDark,
+      themeMode: value!.themeMode,
+      home: Builder(
+        builder: (context) => HomeScreen(
+          maxSlide: MediaQuery.of(context).size.width * 0.835,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      initialRoute: AppRoutes.splash,
+      routes: <String, WidgetBuilder>{
+        AppRoutes.juz: (context) => const JuzIndexScreen(),
+        AppRoutes.splash: (context) => const SplashScreen(),
+        AppRoutes.surah: (context) => const SurahIndexScreen(),
+        AppRoutes.shareApp: (context) => const ShareAppScreen(),
+        AppRoutes.bookmarks: (context) => const BookmarksScreen(),
+        AppRoutes.onboarding: (context) => const OnboardingScreen(),
+        AppRoutes.home: (context) =>
+            HomeScreen(maxSlide: MediaQuery.of(context).size.width * 0.835),
+      },
     );
   }
 }
+
+
